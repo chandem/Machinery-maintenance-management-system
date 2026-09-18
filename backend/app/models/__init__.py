6d08fe529709dcb92bccbfaa95852f5c71378949
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2026-09-18 20:20:29 +0300

    Export work order part model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,9 +1,9 @@
 from .equipment import Equipment, EquipmentCategory, Location, Operator, MeterReading
-from .maintenance import MaintenancePlan, WorkOrder, WorkOrderTask
+from .maintenance import MaintenancePlan, WorkOrder, WorkOrderTask, WorkOrderPart
 from .inventory import Supplier, Part, Inventory, PartTransaction
 
 __all__ = [
     "Equipment", "EquipmentCategory", "Location", "Operator", "MeterReading",
-    "MaintenancePlan", "WorkOrder", "WorkOrderTask",
+    "MaintenancePlan", "WorkOrder", "WorkOrderTask", "WorkOrderPart",
     "Supplier", "Part", "Inventory", "PartTransaction",
 ]

badd577818ac726c3171dbdbe584c2c31ea46591
Chane Eshetu <49794721+chandem@users.noreply.github.com>
2026-09-18 19:16:24 +0300

    Export maintenance models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,7 @@
 from .equipment import Equipment, EquipmentCategory, Location, Operator, MeterReading
+from .maintenance import MaintenancePlan, WorkOrder, WorkOrderTask
 
-__all__ = ["Equipment", "EquipmentCategory", "Location", "Operator", "MeterReading"]
+__all__ = [
+    "Equipment", "EquipmentCategory", "Location", "Operator", "MeterReading",
+    "MaintenancePlan", "WorkOrder", "WorkOrderTask",
+]
